59efead3cfd5ab14d951b140003640a08c5dfdf8
Jeremy Fields <jeremy.fields@viget.com>
2012-04-18 13:02:59 -0600

    Updated console.log fix for IE

--- js/plugins.js
+++ js/plugins.js
@@ -4,6 +4,6 @@
   Author: Jeremy Fields, Viget Labs
 */
 
-// usage: log('inside coolFunc', this, arguments);
-(function(){var b,d,c=this,a=c.console;c.log=b=function(){d.push(arguments);a&&a.log[a.firebug?"apply":"call"](a,Array.prototype.slice.call(arguments))};c.logargs=function(e){b(e,arguments.callee.caller.arguments)};b.history=d=[]})();
+// allows use of console.log
+window.console||(console={log:function(){}});
 
