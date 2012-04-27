9d097012b8525b0e2c674e16fcbbaf0679d21835
Jeremy Fields <jeremy.fields@viget.com>
2012-04-27 12:38:16 -0600

    Moved loadScripts into self executing function

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -11,10 +11,11 @@ var PB = PB || {};
 	
 	PB.controller = body.getAttribute('data-controller').toLowerCase();
 	PB.action = body.getAttribute('data-action').toLowerCase();
+	
+	// function for loading the JS package
+	PB.loadScripts = function(controller,action) {
+		$LAB
+		.script([base.loadfirst.depends],base.loadfirst.url).wait() // load and run the global scripts first
+		.script([pages[controller][action].depends],pages[controller][action].url);
+	};
 })();
-
-PB.loadScripts = function(controller,action) {
-	$LAB
-	.script([base.loadfirst.depends],base.loadfirst.url).wait() // load and run the global scripts first
-	.script([pages[controller][action].depends],pages[controller][action].url);
-};
